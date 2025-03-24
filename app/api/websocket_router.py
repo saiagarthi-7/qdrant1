3ac442f558679f49e34d35a0a64b6c1ba3d4c3bb
saiagarthi-7 <saimanikya.agarthi@aifalabs.com>
2025-03-24 12:26:11 +0530

    Implemented websocket

--- app/api/websocket_router.py
+++ app/api/websocket_router.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, WebSocket
+from fastapi import APIRouter, WebSocket,WebSocketDisconnect
 from core.chatbot import generate_answer
 
 router = APIRouter()
@@ -6,7 +6,10 @@ router = APIRouter()
 @router.websocket("/ws/faq")
 async def websocket_endpoint(wbsock: WebSocket):
     await wbsock.accept()
-    while True:
-        query = await wbsock.receive_text()
-        answer = generate_answer(query)
-        await wbsock.send_text(answer)
+    try:
+        while True:
+            query = await wbsock.receive_text()
+            answer = generate_answer(query)
+            await wbsock.send_text(answer)
+    except WebSocketDisconnect:
+        print("Client disconnected")
--- app/main.py
+++ app/main.py
@@ -1,10 +1,20 @@
 from fastapi import FastAPI
 from api import websocket_router
 from core.qdrant_service import search_faq
+from fastapi.middleware.cors import CORSMiddleware
+
 
 app = FastAPI(title="FAQ Chatbot API")
 
 app.include_router(websocket_router.router)
+
+app.add_middleware(      
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def read_root():
